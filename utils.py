03d68fb3b9acd8a6ec6c9be9579840b8186bbac9
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2026-06-25 17:18:54 -0500

    feat: Agregar contenido a utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+# Este archivo contendrá todos los datos no variables o constantes que tendrá el código.
+
+from tkinter import font # Importación módulo específico de fuentes
+
+# Configuración principal de la ventana
+ANCHO_VENTANA = 700
+ALTO_VENTANA = 400
+
+# Colores
+
+
+# Fuentes
+F_T_P = font.Font(family="Helvetica",size=20, weigth="bold",slant="roman")
+F_S_P = font.Font(family="Helvetica",size=16, weigth="bold",slant="roman")
+F_L_P = font.Font(family="Helvetica",size=12, weigth="bold",slant="roman")
